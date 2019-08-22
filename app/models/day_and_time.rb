4b6307f40696d6d9e03fa2fd4fe1ff29e1f55483
shane <smp1993@gmail.com>
2019-08-21 22:30:22 -0400

    added friendly datetime

--- app/models/day_and_time.rb
+++ app/models/day_and_time.rb
@@ -3,4 +3,8 @@ class DayAndTime < ApplicationRecord
   has_many :attendees
   has_many :users, through: :attendees
   belongs_to :gym 
+
+  def friendly_datetime
+    datetime.strftime("%a, %d %b %Y %l:%M %p")
+  end
 end
